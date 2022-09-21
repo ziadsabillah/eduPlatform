5f130542b44b803a90f38816c60f326413f65701
Ziad Sabillah <ziadoox.sabillah@gmail.com>
2022-09-21 13:01:39 +0100

    reinitialized with nextauth and prisma

---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+export const Nav = () => {
+  // simple navigation with tailwindcss that has a home and login links
+  return (
+    <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <span className="font-semibold text-xl tracking-tight">
+          Edu Platform
+        </span>
+      </div>
+      <div className="block lg:hidden">
+        <button className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white">
+          <svg
+            className="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="text-sm lg:flex-grow">
+          <Link href="/">
+            <a className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
+              Home
+            </a>
+          </Link>
+          <Link href="/login">
+            <a className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
+              Login
+            </a>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
